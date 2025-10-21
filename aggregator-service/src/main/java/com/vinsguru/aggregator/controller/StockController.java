83c1819e17498b67781f947cc76761b243a293a9
mrpaulwoods <mr.paul.woods@gmail.com>
2025-10-20 19:37:18 -0500

    feat: initialize price update subscription and refactor SSE handling
    - Added `PriceUpdateSubscriptionInitializer` to subscribe to price updates using gRPC on application startup.
    - Refactored `StockController` to delegate SSE emitter creation to `PriceUpdateListener`.

--- aggregator-service/src/main/java/com/vinsguru/aggregator/controller/StockController.java
+++ aggregator-service/src/main/java/com/vinsguru/aggregator/controller/StockController.java
@@ -1,40 +1,23 @@
 package com.vinsguru.aggregator.controller;
 
-import com.google.common.util.concurrent.Uninterruptibles;
+import com.vinsguru.aggregator.service.PriceUpdateListener;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
-import java.io.IOException;
-import java.util.concurrent.TimeUnit;
-
 @RestController
 @RequestMapping("stock")
 public class StockController {
 
-    @GetMapping(value="updates", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    @Autowired
+    private PriceUpdateListener listener;
+
+    @GetMapping(value = "updates", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
     public SseEmitter priceUpdates() {
-
-        SseEmitter emitter = new SseEmitter(2000L);
-
-        Runnable runnable = () -> {
-            for (int i = 0; i < 5; i++) {
-                Uninterruptibles.sleepUninterruptibly(1, TimeUnit.SECONDS);
-                try {
-                    emitter.send("hello-" + i);
-                } catch (IOException e) {
-                    throw new RuntimeException(e);
-                }
-            }
-
-            emitter.complete();
-        };
-
-        Thread.ofVirtual().start(runnable);
-
-        return emitter;
+        return listener.createEmitter();
     }
 
 }
